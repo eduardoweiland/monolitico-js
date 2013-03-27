fc7444cc74c4e9425f0f3a8fe7ed5635fd12f0ba
Eduardo Weiland <duduweiland@users.sourceforge.net>
2013-03-26 21:32:09 -0300

    Arquivos que faltaram commitar

--- js/program.js
+++ js/program.js
@@ -2,7 +2,20 @@ function Program() {
     this.init.apply(this, arguments);
 }
 
+// -- closure -- Variáveis privadas da classe Program
+(function() {
+
+var rawcontent = ''; // conteúdo original do programa (como recebido no construtor)
+
 Program.prototype = {
+
+    /**
+     * Vetor de instruções.
+     * Armazena todas as instruções rotuladas do programa. Os dados são
+     * armazenados como objetos do tipo Instruction.
+     */
+    instructions: [],
+
     /**
      * Cria um novo programa monolítico a partir de um conjunto de instruções
      * rotuladas compostas. No primeiro momento, são aceitas apenas instruções
@@ -12,7 +25,8 @@ Program.prototype = {
      *        rotuladas compostas no formato abreviado
      */
     init: function(content) {
-        this.rawcontent = content || '';
+        rawcontent = content || '';
+        this.instructions = Program.parse(rawcontent);
     },
 
     /**
@@ -20,13 +34,38 @@ Program.prototype = {
      * estuturalmente.
      *
      * @return Boolean Se o programa é válido ou não.
+     *
+     * @public
      */
     isValid: function() {
-        if (this.rawcontent) {
+        if (rawcontent) {
             // TODO: validação
             return true;
         }
 
         return false;
+    },
+
+    /**
+     * Analisa o conteúdo de @a content e retorna um vetor de Instruction's
+     * existentes.
+     *
+     * @param String conteúdo a ser analisado.
+     * @return Vetor de Instruction's analisadas presentes no programa.
+     *
+     * @public
+     * @static
+     */
+    parse: function(content) {
+        var lines = content.split('\n'),
+            instructions = [], i;
+
+        for (i = 0; i < lines.length; ++i) {
+            instructions.push(new Instruction(lines[i]));
+        }
+
+        return instructions;
     }
 }
+
+})();   // -- end Program closure
