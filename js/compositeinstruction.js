e5716bda0d0fcd51c24287384ef13d82800345ac
Eduardo Weiland <duduweiland@users.sourceforge.net>
2013-04-20 14:51:58 -0300

    Criando classe para instrução simples

---
+++ js/compositeinstruction.js
@@ -0,0 +1,148 @@
+function CompositeInstruction() {
+    this.init.apply(this, arguments);
+}
+
+// -- static -- Propriedades e métodos estáticos da classe
+
+CompositeInstruction.TYPE_OPERATION    = 1;   // instrução comum (testes e operações, sem ciclos nem paradas)
+CompositeInstruction.TYPE_STOP         = 2;   // instrução que contém uma parada
+CompositeInstruction.TYPE_CYCLE        = 3;   // instrução que contém um ciclo
+CompositeInstruction.TYPE_FORMAL_STOP  = 4;   // instrução de parada para formalização (final do programa)
+CompositeInstruction.TYPE_FORMAL_CYCLE = 5;   // instrução de ciclo para formalização (final do programa)
+
+CompositeInstruction.LABEL_STOP_VALUE  = '&'; // valor para o rótulo de parada
+CompositeInstruction.LABEL_CYCLE_VALUE = 'w'; // valor para o rótulo de ciclo
+
+CompositeInstruction.LABEL_STOP_UTF8   = 'ε'; // representação do rótulo de parada (apenas para exibição)
+CompositeInstruction.LABEL_CYCLE_UTF8  = 'ω'; // representação do rótulo de ciclo (apenas para exibição)
+
+/**
+ * Expressão regular utilizada para validar a string que representa a instrução.
+ */
+CompositeInstruction.VALID_REGEX = /^([0-9w&]+):\((.+),([0-9w&]+)\),\((.+),([0-9w&]+)\)$/;
+
+/**
+ * Analisa o conteúdo de @a content e retorna um vetor com os "pedaços" da
+ * instrução ou false se o conteúdo recebido for inválido.
+ *
+ * O vetor retornado possui cinco String's na seguinte ordem:
+ *   índice 0 = rótulo da operação
+ *   índice 1 = primeira operação
+ *   índice 2 = primeiro rótulo sucessor
+ *   índice 3 = segunda operação
+ *   índice 4 = segundo rótulo sucessor
+ *
+ * Ex.: para o conteúdo   3:(G,4),(F,5)
+ *     os índices serão   0  1 2   3 4
+ *
+ * @param String content O conteúdo a ser analisado (espaços serão desconsiderados)
+ * @return Array|Boolean O vetor com os pedaços da instrução já separados.
+ *
+ * @public
+ * @static
+ */
+CompositeInstruction.parse = function(content) {
+    // ignora espaços e separa os "pedaços" da instrução
+    var pieces = content.replace(/ /g, '').match(CompositeInstruction.VALID_REGEX);
+
+    // Validação: verifica apenas o formato geral (não os valores)
+    if (!(pieces && $.isArray(pieces) && pieces.length == 6)) {
+        // TODO: classe de erro específica para o projeto
+        throw new Error('Instrução inválida: ' + content);
+        return false;
+    }
+
+    // o índice 0 contém a instrução completa que é desnecessária, então remove
+    return pieces.slice(1);
+};
+
+// -- prototype -- Propriedades e métodos acessíveis pelo objeto
+CompositeInstruction.prototype = {
+
+    /**
+     * Conteúdo original recebido no construtor.
+     */
+    rawcontent: '',
+
+    /**
+     * Tipo da instrução.
+     * Armazena o tipo da instrução (operação, parada, ciclo, etc.).
+     */
+    type: null,
+
+    /**
+     * Rótulo da instrução.
+     * Armazena o rótulo da instrução, no formato recebido (String).
+     */
+    label: null,
+
+    firstOperation: null,
+
+    firstSucessor: null,
+
+    secondOperation: null,
+
+    secondSucessor: null,
+
+    /**
+     * Cria uma nova instrução de um programa monolítico a partir de uma string
+     * contendo a definição da instruçção no seguinte formato:
+     *            1:(F,2),(G,3)
+     *
+     * @param String content A definição da instrução
+     * @see Instruction.VALID_REGEX
+     */
+    init: function(content) {
+        this.rawcontent = content || '';
+
+        try {
+            var pieces = CompositeInstruction.parse(this.rawcontent);
+            this.label = pieces[0];
+            this.firstOperation  = pieces[1];
+            this.firstSucessor   = pieces[2];
+            this.secondOperation = pieces[3];
+            this.secondSucessor  = pieces[4];
+
+            this.type = CompositeInstruction.TYPE_OPERATION;
+        }
+        catch (e) {
+            console.error(e.message);
+        }
+    },
+
+    /**
+     * Verifica se a instrução como está definida atualmente é válida
+     * estuturalmente.
+     *
+     * @return Boolean Se a instrução é válida ou não.
+     *
+     * @public
+     */
+    isValid: function() {
+        // sem conteúdo ou conteúdo inválido
+        if (typeof(this.rawcontent) !== 'string') {
+            return false;
+        }
+
+        // verifica se o tipo é válido
+        switch (this.type) {
+            case CompositeInstruction.TYPE_OPERATION:
+            case CompositeInstruction.TYPE_STOP:
+            case CompositeInstruction.TYPE_CYCLE:
+            case CompositeInstruction.TYPE_FORMAL_STOP:
+            case CompositeInstruction.TYPE_FORMAL_CYCLE:
+                break;  // OK
+            default:
+                return false;   // tipo inválido
+        }
+
+        // O rótulo deve ser a parada OU o ciclo OU um número decimal válido
+        if (this.label !== CompositeInstruction.LABEL_STOP_VALUE
+            && this.label !== CompositeInstruction.LABEL_CYCLE_VALUE
+            && isNaN(parseInt(this.label, 10))) {
+            return false;
+        }
+
+        return true;
+    }
+};
--- js/instruction.js
+++ js/instruction.js
@@ -1,148 +1,81 @@
-function Instruction() {
+function SimpleInstruction() {
     this.init.apply(this, arguments);
 }
 
-// -- static -- Propriedades e métodos estáticos da classe
+SimpleInstruction.TYPE_OPERATION = 0;
+SimpleInstruction.TYPE_TEST      = 1;
 
-Instruction.TYPE_OPERATION    = 1;   // instrução comum (testes e operações, sem ciclos nem paradas)
-Instruction.TYPE_STOP         = 2;   // instrução que contém uma parada
-Instruction.TYPE_CYCLE        = 3;   // instrução que contém um ciclo
-Instruction.TYPE_FORMAL_STOP  = 4;   // instrução de parada para formalização (final do programa)
-Instruction.TYPE_FORMAL_CYCLE = 5;   // instrução de ciclo para formalização (final do programa)
-
-Instruction.LABEL_STOP_VALUE  = '&'; // valor para o rótulo de parada
-Instruction.LABEL_CYCLE_VALUE = 'w'; // valor para o rótulo de ciclo
-
-Instruction.LABEL_STOP_UTF8   = 'ε'; // representação do rótulo de parada (apenas para exibição)
-Instruction.LABEL_CYCLE_UTF8  = 'ω'; // representação do rótulo de ciclo (apenas para exibição)
-
-/**
- * Expressão regular utilizada para validar a string que representa a instrução.
- */
-Instruction.VALID_REGEX = /^([0-9w&]+):\((.+),([0-9w&]+)\),\((.+),([0-9w&]+)\)$/;
-
-/**
- * Analisa o conteúdo de @a content e retorna um vetor com os "pedaços" da
- * instrução ou false se o conteúdo recebido for inválido.
- *
- * O vetor retornado possui cinco String's na seguinte ordem:
- *   índice 0 = rótulo da operação
- *   índice 1 = primeira operação
- *   índice 2 = primeiro rótulo sucessor
- *   índice 3 = segunda operação
- *   índice 4 = segundo rótulo sucessor
- *
- * Ex.: para o conteúdo   3:(G,4),(F,5)
- *     os índices serão   0  1 2   3 4
- *
- * @param String content O conteúdo a ser analisado (espaços serão desconsiderados)
- * @return Array|Boolean O vetor com os pedaços da instrução já separados.
- *
- * @public
- * @static
- */
-Instruction.parse = function(content) {
-    // ignora espaços e separa os "pedaços" da instrução
-    var pieces = content.replace(/ /g, '').match(Instruction.VALID_REGEX);
-
-    // Validação: verifica apenas o formato geral (não os valores)
-    if (!(pieces && $.isArray(pieces) && pieces.length == 6)) {
-        // TODO: classe de erro específica para o projeto
-        throw new Error('Instrução inválida: ' + content);
-        return false;
-    }
-
-    // o índice 0 contém a instrução completa que é desnecessária, então remove
-    return pieces.slice(1);
-};
-
-// -- prototype -- Propriedades e métodos acessíveis pelo objeto
-Instruction.prototype = {
+SimpleInstruction.prototype = {
 
     /**
-     * Conteúdo original recebido no construtor.
-     */
-    rawcontent: '',
-
-    /**
-     * Tipo da instrução.
-     * Armazena o tipo da instrução (operação, parada, ciclo, etc.).
+     * Tipo da instrução representada por esse objeto.
+     * @type Number
      */
     type: null,
 
     /**
-     * Rótulo da instrução.
-     * Armazena o rótulo da instrução, no formato recebido (String).
+     * Operação executada nessa instrução. Válido apenas quando o tipo for
+     * operação.
+     * @type String
      */
-    label: null,
-
-    firstOperation: null,
-
-    firstSucessor: null,
-
-    secondOperation: null,
-
-    secondSucessor: null,
+    operation: null,
 
     /**
-     * Cria uma nova instrução de um programa monolítico a partir de uma string
-     * contendo a definição da instruçção no seguinte formato:
-     *            1:(F,2),(G,3)
-     *
-     * @param String content A definição da instrução
-     * @see Instruction.VALID_REGEX
+     * Rótulo ao qual o programa será redirecionado após executar a operação.
+     * Válido apenas quando o tipo for operação.
+     * @type Number
      */
-    init: function(content) {
-        this.rawcontent = content || '';
-
-        try {
-            var pieces = Instruction.parse(this.rawcontent);
-            this.label = pieces[0];
-            this.firstOperation  = pieces[1];
-            this.firstSucessor   = pieces[2];
-            this.secondOperation = pieces[3];
-            this.secondSucessor  = pieces[4];
-
-            this.type = Instruction.TYPE_OPERATION;
-        }
-        catch (e) {
-            console.error(e.message);
-        }
-    },
+    nextLabel: null,
 
     /**
-     * Verifica se a instrução como está definida atualmente é válida
-     * estuturalmente.
-     *
-     * @return Boolean Se a instrução é válida ou não.
-     *
-     * @public
+     * Nome do teste a ser executado. Válido apenas quando o tipo for teste.
+     * @type String
      */
-    isValid: function() {
-        // sem conteúdo ou conteúdo inválido
-        if (typeof(this.rawcontent) !== 'string') {
-            return false;
+    testName: null,
+
+    /**
+     * Rótulo ao qual o programa será redirecionado quando o teste for verdadeiro.
+     * Válido apenas quando o tipo for teste.
+     * @type Number
+     */
+    trueLabel: null,
+
+    /**
+     * Rótulo ao qual o programa será redirecionado quando o teste for falso.
+     * Válido apenas quando o tipo for teste.
+     * @type Number
+     */
+    falseLabel: null,
+
+    /**
+     * Construtor. Criar um nova instrução simples do tipo @a type.
+     *
+     * Os parâmetros aceitos pelo construtor variam de acordo com o tipo de
+     * instrução sendo criada.
+     *
+     * Para criar uma instrução do tipo operação:
+     * @param String operation Nome da operação a ser executada
+     * @param Number nextLabel Rótulo a ser seguido após a operação
+     *
+     * Para criar uma instrução do tipo teste:
+     * @param String testName   Nome do teste a ser executado
+     * @param Number trueLabel  Rótulo a ser seguido se o teste for verdadeiro
+     * @param Number falseLabel Rótulo a ser seguido se o teste for falso
+     */
+    init: function(type) {
+        if (type === SimpleInstruction.TYPE_OPERATION && arguments.length === 3) {
+            this.operation = arguments[1];
+            this.nextLabel = arguments[2];
+        }
+        else if (type === SimpleInstruction.TYPE_TEST && arguments.length === 4) {
+            this.testName   = arguments[1];
+            this.trueLabel  = arguments[2];
+            this.falseLabel = arguments[3];
+        }
+        else {
+            throw new Error("Invalid instruction type.");
         }
 
-        // verifica se o tipo é válido
-        switch (this.type) {
-            case Instruction.TYPE_OPERATION:
-            case Instruction.TYPE_STOP:
-            case Instruction.TYPE_CYCLE:
-            case Instruction.TYPE_FORMAL_STOP:
-            case Instruction.TYPE_FORMAL_CYCLE:
-                break;  // OK
-            default:
-                return false;   // tipo inválido
-        }
-
-        // O rótulo deve ser a parada OU o ciclo OU um número decimal válido
-        if (this.label !== Instruction.LABEL_STOP_VALUE
-            && this.label !== Instruction.LABEL_CYCLE_VALUE
-            && isNaN(parseInt(this.label, 10))) {
-            return false;
-        }
-
-        return true;
+        this.type = type;
     }
 };
